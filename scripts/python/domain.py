01d377f161476e03d1ea05c41bcd745a494737ab
Bruno Sant'Anna <bruno@btech.software>
2020-07-01 12:53:18 +0200

    Usage of PYTHONPATH environment variable and FeatureProducer class

--- scripts/python/domain.py
+++ scripts/python/domain.py
@@ -1,13 +1,15 @@
 import json
 
 
-class LaneDetectionFeatures:
+class FeatureProducer:
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+class LaneDetectionFeatures(FeatureProducer):
     def __init__(self, left_fit, right_fit):
         if left_fit is not None:
             self.left_fit = list(left_fit)
 
         if right_fit is not None:
             self.right_fit = list(right_fit)
-
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
